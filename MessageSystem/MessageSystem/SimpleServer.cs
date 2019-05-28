00f2365dade408cf5945b9335e59b1670af41c0a
unknown <maciek2608@gmail.com>
2019-05-28 13:02:05 +0200

    Project files

---
+++ MessageSystem/MessageSystem/SimpleServer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+
+namespace MessageSystem
+{
+    class SimpleServer
+    {
+        Server server;
+        ClientInfo client;
+        void Start()
+        {
+            server = new Server(2345, new ClientEvent(ClientConnect));
+        }
+
+        bool ClientConnect(Server serv, ClientInfo new_client)
+        {
+            new_client.Delimiter = '\n';
+            new_client.OnRead += new ConnectionRead(ReadData);
+            return true; // allow this connection
+        }
+
+        void ReadData(ClientInfo ci, String text)
+        {
+            Console.WriteLine("Received from " + ci.ID + ": " + text);
+            if (text[0] == '!')
+                server.Broadcast(Encoding.UTF8.GetBytes(text));
+            else ci.Send(text);
+        }
+    }
+}
---
+++ MessageSystem/SimpleServer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class SimpleServer
+{
+    Server server;
+    ClientInfo client;
+    void Start()
+    {
+        server = new Server(2345, new ClientEvent(ClientConnect));
+    }
+
+    bool ClientConnect(Server serv, ClientInfo new_client)
+    {
+        new_client.Delimiter = '\n';
+        new_client.OnRead += new ConnectionRead(ReadData);
+        return true; // allow this connection
+    }
+
+    void ReadData(ClientInfo ci, String text)
+    {
+        Console.WriteLine("Received from " + ci.ID + ": " + text);
+        if (text[0] == '!')
+            server.Broadcast(Encoding.UTF8.GetBytes(text));
+        else ci.Send(text);
+    }
+}
